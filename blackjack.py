ca8ac5874beb37880c3edf68462e86cc10f1de52
Ethan Ospina <ethan_ospina@yahoo.com>
2022-07-29 22:25:39 -0400

    In blackjack.py, I took out the game function and turned it back into a while loop. I then created a new function for the action of checking the game condition to see if anyone won. I moved the players hands to a more friendly spot on the script so its visible at all times. I cleaned up the stay section and added the dealer has to draw if they are under 17 logic. Finally I fixed the restart option to be able to hit more than once before the game ended.

--- blackjack.py
+++ blackjack.py
@@ -1,61 +1,73 @@
 game_on =True
 import os
-def blackjackmain():
-    global game_on
-    global deck_list
+import random
+
+print("Welcome to Blackjack!")
+print("The goal of the game is to get or get as close as you can to 21 without going over!")
+deck = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "J": 10, "Q": 10, "K": 10}
+deck_list = [1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+randomnum = random.randint(0,len(deck_list)-1)
+card_choice = deck_list[randomnum]
+
+def draw_a_card():
+    """give a new random card everytime its called"""
     global deck
-    import random
-    import os
-    print("Welcome to Blackjack!")
-    print("The goal of the game is to get or get as close as you can to 21 without going over!")
-    deck = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "J": 10, "Q": 10, "K": 10}
-    deck_list = [1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+    global deck_list
     randomnum = random.randint(0,len(deck_list)-1)
     card_choice = deck_list[randomnum]
-
-    def draw_a_card():
-        """give a new random card everytime its called"""
-        global deck
-        global deck_list
-        randomnum = random.randint(0,len(deck_list)-1)
-        card_choice = deck_list[randomnum]
-        return card_choice
+    return card_choice
 
 
-    user_hand = []
-    comp_hand = []
-
-    def make_hand(hand, cards):
-        """Takes the random card and puts it in your hand"""
-        hand.append(cards)
-        return hand
-
-    def sum_total(hand, total):
-        """takes the user/comp hand and adds the cards up in the hand to display the total"""
-        global deck
-        for card in hand:
-            total += deck[card]
-        return (total)
 
 
-    user_total = 0
-    comp_total = 0
+def make_hand(hand, cards):
+    """Takes the random card and puts it in your hand"""
+    hand.append(cards)
+    return hand
 
-    print("Heres is your starting hand:")
-    make_hand(user_hand, draw_a_card())
-    print(make_hand(user_hand, draw_a_card()))
-    print("Here is the computers starting hand:")
-    show = make_hand(comp_hand, draw_a_card())
-    make_hand(comp_hand, draw_a_card())
-    print(f"['XX', '{comp_hand[1]}']")
+def sum_total(hand, total):
+    """takes the user/comp hand and adds the cards up in the hand to display the total"""
+    global deck
+    for card in hand:
+        total += deck[card]
+    return (total)
 
-    user_total = sum_total(user_hand, user_total)
-    comp_total = sum_total(comp_hand, comp_total)
-    print(f"Your hand total is: {user_total}\n")
+def card_checker(user_total, comp_total):
+    """Compares the user score and computer score to see if anyone won or lost"""
+    global game_on
+    print(f"Your total: {user_total} v.s computer total: {comp_total}")
+    if user_total > comp_total:
+        print("You win!")
+        game_on = False
+    elif user_total == comp_total:
+        print("It's a draw")
+        game_on = False
+    else:
+        print("You lose")
+        game_on = False
 
-    choice = int(input("Press [1] to Hit or [2] to stay: "))
+user_hand = []
+comp_hand = []
+user_total = 0
+comp_total = 0
 
-        
+print("Heres is your starting hand:")
+make_hand(user_hand, draw_a_card())
+print(make_hand(user_hand, draw_a_card()))
+print("Here is the computers starting hand:")
+show = make_hand(comp_hand, draw_a_card())
+make_hand(comp_hand, draw_a_card())
+print(f"['XX', '{comp_hand[1]}']")
+
+user_total = sum_total(user_hand, user_total)
+comp_total = sum_total(comp_hand, comp_total)
+print(f"Your hand total is: {user_total}\n")
+
+choice = int(input("Press [1] to Hit or [2] to stay: "))
+game_on = True
+while game_on:
+
+    
     if choice == 1:
         new_card = draw_a_card()
         user_hand = make_hand(user_hand, new_card)
@@ -70,60 +82,44 @@ def blackjackmain():
         elif user_total>21 or comp_total>21:
             print("It's a Bust!")
             game_on = False
-        #choice = int(input("Press [1] to Hit or [2] to stay: "))
+        if game_on == True:
+            choice = int(input("Press [1] to Hit or [2] to stay: "))
     elif choice == 2:
-        print(f"Your total: {user_total} v.s computer total: {comp_total}")
-        if user_total > comp_total:
-            print("You win!")
+        if comp_total < 17:
+            new_comp_card = draw_a_card()
+            comp_hand = make_hand(comp_hand, new_comp_card)
+            comp_total += deck[new_comp_card]
+        card_checker(user_total, comp_total)
+    
+    if game_on == False:
+        restart = int(input("Press [1] to play again or [2] to exit to the Game Room. "))
+        if restart == 2:
+            print("Thanks for playing!")
             game_on = False
-        elif user_total == comp_total:
-            print("It's a draw")
-            game_on = False
-        else:
-            print("You lose")
-            game_on = False
-    # replay = int(input("Type [1] to play again or [2] to exit: "))
-    ##os.system('clear')
-    # if replay == 1:
-    #     game_on = True
-    #     user_hand = []
-    #     comp_hand = []
-    #     user_total = 0
-    #     comp_total = 0
+            os.system('clear')
+            os.system('python3 main.py')
+        elif restart == 1:
+            game_on = True
+            os.system('clear')
+            user_hand = []
+            comp_hand = []
+            user_total = 0
+            comp_total = 0
 
-    #     print("Heres is your starting hand:")
-    #     make_hand(user_hand, draw_a_card())
-    #     print(make_hand(user_hand, draw_a_card()))
-    #     print("Here is the computers starting hand:")
-    #     show = make_hand(comp_hand, draw_a_card())
-    #     make_hand(comp_hand, draw_a_card())
-    #     print(f"['XX', '{comp_hand[1]}']")
+            print("Heres is your starting hand:")
+            make_hand(user_hand, draw_a_card())
+            print(make_hand(user_hand, draw_a_card()))
+            print("Here is the computers starting hand:")
+            show = make_hand(comp_hand, draw_a_card())
+            make_hand(comp_hand, draw_a_card())
+            print(f"['XX', '{comp_hand[1]}']")
 
-    #     user_total = sum_total(user_hand, user_total)
-    #     comp_total = sum_total(comp_hand, comp_total)
-    #     print(f"Your hand total is: {user_total}\n")
+            user_total = sum_total(user_hand, user_total)
+            comp_total = sum_total(comp_hand, comp_total)
+            print(f"Your hand total is: {user_total}\n")
 
-
-
-    #     choice = int(input("Press [1] to Hit or [2] to stay: "))
-
-        
-    # else:
-    #     print("Thanks for playing!")
-
-
-
-while game_on:
-    blackjackmain()
-    restart = int(input("Press [1] to play again or [2] to exit to the Game Room. "))
-    if restart == 2:
-        print("Thanks for playing!")
-        game_on = False
-        os.system('clear')
-        os.system('python3 main.py')
-    elif restart == 1:
-        os.system('clear')
-        game_on = True
+            choice = int(input("Press [1] to Hit or [2] to stay: "))
+            
 
 
 
